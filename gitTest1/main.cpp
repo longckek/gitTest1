a1023f8bb59492c53ba99e0b15551a59699047a4
Lan Chu <hoanglan.ict@gmail.com>
2021-01-14 21:29:44 +0700

    Initial Commit

---
+++ gitTest1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gitTest1
+//
+//  Created by Lan Chu on 1/14/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
